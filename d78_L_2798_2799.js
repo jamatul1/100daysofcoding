5ffb766dd8c5b42d725bf8e4f9501aab6c439866
Jamatul Islam <newstartjam@gmail.com>
2023-07-30 21:40:42 +0600

    day78-modified

--- d78_L_2798_2799.js
+++ d78_L_2798_2799.js
@@ -1,24 +1,37 @@
-function partition(start, end) {
-  const pivot = fn(arr[end]);
-  let i = start - 1;
-  for (let j = start; j <= end - 1; j++) {
-    if (fn(arr[j]) <= pivot) {
-      swap(++i, j);
-    }
+// Weekly Contest - 356 - Solved 2
+//---2798. Number of Employees Who Met the Target----
+var numberOfEmployeesWhoMetTarget = function (hours, target) {
+  let count = 0;
+  for (let i of hours) {
+    if (i >= target) count++;
   }
-  swap(++i, end);
-  return i;
-}
-function swap(i, j) {
-  [arr[i], arr[j]] = [arr[j], arr[i]];
-}
-function quick_sort(start, end) {
-  if (start < end) {
-    let pi = partition(start, end);
-    quick_sort(start, pi - 1);
-    quick_sort(pi + 1, end);
-  }
-}
+  return count;
+};
 
-quick_sort(0, arr.length - 1);
-return arr;
+//----2799. Count Complete Subarrays in an Array---
+var countCompleteSubarrays = function (nums) {
+  let n = (n = nums.length);
+  let dMap = new Map();
+  for (let i = 0; i < n; ++i) dMap.set(nums[i], 1);
+  let k = dMap.size;
+  let map = new Map();
+  let ans = 0,
+    right = 0,
+    window = 0;
+
+  for (let left = 0; left < n; left++) {
+    while (right < n && window < k) {
+      if (map.has(nums[right])) map.set(nums[right], map.get(nums[right]) + 1);
+      else map.set(nums[right], 1);
+
+      if (map.get(nums[right]) == 1) window++;
+
+      right++;
+    }
+    if (window == k) ans += n - right + 1;
+    if (map.has(nums[left])) map.set(nums[left], map.get(nums[left]) - 1);
+    if (map.get(nums[left]) == 0) --window;
+  }
+
+  return ans;
+};
